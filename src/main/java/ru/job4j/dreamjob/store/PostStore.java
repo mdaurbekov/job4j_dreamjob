c75b21cb4c76191af90b2dcdcf48167d1e5470ee
mdaurbekov <dreamkov@gmail.com>
2022-11-23 15:29:38 +0300

    task_504841

---
+++ src/main/java/ru/job4j/dreamjob/store/PostStore.java
@@ -0,0 +1,29 @@
+package ru.job4j.dreamjob.store;
+
+import ru.job4j.dreamjob.model.Post;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class PostStore {
+
+    private static final PostStore INST = new PostStore();
+
+    private final Map<Integer, Post> posts = new ConcurrentHashMap<>();
+
+    private PostStore() {
+        posts.put(1, new Post(1, "Junior Java Job", "description_1", LocalDateTime.now()));
+        posts.put(2, new Post(2, "Middle Java Job", "description_2", LocalDateTime.now()));
+        posts.put(3, new Post(3, "Senior Java Job", "description_3", LocalDateTime.now()));
+    }
+
+    public static PostStore instOf() {
+        return INST;
+    }
+
+    public Collection<Post> findAll() {
+        return posts.values();
+    }
+}
