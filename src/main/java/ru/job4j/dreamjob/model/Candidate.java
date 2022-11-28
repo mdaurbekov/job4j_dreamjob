aeb8ee9bc97f8d606cd892c7ccd709f13207cd92
mdaurbekov <dreamkov@gmail.com>
2022-11-28 15:12:13 +0300

    task_504853

--- src/main/java/ru/job4j/dreamjob/model/Candidate.java
+++ src/main/java/ru/job4j/dreamjob/model/Candidate.java
@@ -8,6 +8,7 @@ public class Candidate {
     private String name;
     private String description;
     private LocalDateTime created;
+    private boolean visible;
 
     public Candidate() {
     }
@@ -49,6 +50,14 @@ public class Candidate {
 
     public void setCreated(LocalDateTime created) {
         this.created = created;
+    }
+
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
     }
 
     @Override
--- src/main/java/ru/job4j/dreamjob/model/Post.java
+++ src/main/java/ru/job4j/dreamjob/model/Post.java
@@ -8,6 +8,7 @@ public class Post {
     private String name;
     private String description;
     private LocalDateTime created;
+    private boolean visible;
 
     public Post() {
     }
@@ -50,6 +51,13 @@ public class Post {
 
     public void setCreated(LocalDateTime created) {
         this.created = created;
+    }
+    public boolean isVisible() {
+        return visible;
+    }
+
+    public void setVisible(boolean visible) {
+        this.visible = visible;
     }
 
     @Override
