994faaa90ade44617ee677cdc03035a9b999b068
mdaurbekov <dreamkov@gmail.com>
2022-11-28 14:58:52 +0300

    task_504857

--- src/main/java/ru/job4j/dreamjob/store/CandidateStore.java
+++ src/main/java/ru/job4j/dreamjob/store/CandidateStore.java
@@ -1,5 +1,6 @@
 package ru.job4j.dreamjob.store;
 
+import net.jcip.annotations.ThreadSafe;
 import org.springframework.stereotype.Repository;
 import ru.job4j.dreamjob.model.Candidate;
 
@@ -8,7 +9,7 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
-
+@ThreadSafe
 @Repository
 public class CandidateStore {
 
